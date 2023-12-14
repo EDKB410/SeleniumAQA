3cd928cb9997ba3adc5857a6d173ebc6fc23afb4
solovyev.ed <evgeniy.solovyev@sbermarket.ru>
2023-12-14 21:00:01 +0300

    Demo tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,43 @@
 import pytest
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
 from selenium.webdriver.firefox.service import Service as FFService
 from selenium.webdriver.firefox.options import Options as FFOptions
 
-def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--url", action="store", default="http://192.168.1.64:8081")
+from selenium.webdriver.safari.service import Service as SafariService
+from selenium.webdriver.safari.options import Options as SafariOptions
+
 
 @pytest.fixture()
 def browser(request):
-    browser_name = request.config.getoption("--browser")
-    url = request.config.getoption("--url")
+    browser_name = request.config.getoption("--browser", default="chrome")
+    url = request.config.getoption("--url", default="http://192.168.1.64:8081")
 
     if browser_name == "chrome":
-        driver = webdriver.Chrome(service=ChromiumService())
-    elif browser_name == "firefox":
-        driver = webdriver.Firefox(options=FFOptions(), service=FFService())
-    else:
-        driver = webdriver.Safari()
+        service = ChromeService()
+        options = ChromeOptions()
+        options.add_argument("headless=new")  # Отключение визуализации браузера
+        driver = webdriver.Chrome(service=service, options=options)
 
-    driver.maximize_window()
+    elif browser_name == "firefox":
+        service = FFService()
+        options = FFOptions()
+        options.add_argument("-headless")  # Отключение визуализации браузера
+        driver = webdriver.Firefox(service=service, options=options)
+
+    else:
+        service = SafariService()
+        options = SafariOptions()
+        options.add_argument("-headless")  # Отключение визуализации браузера
+        driver = webdriver.Safari(service=service, options=options)
+
+    # Set window size
+    width = 1920  # in pixels
+    height = 1080  # in pixels
+    driver.set_window_size(width, height)
 
     request.addfinalizer(driver.close)
 
