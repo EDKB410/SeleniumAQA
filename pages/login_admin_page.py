a70e3110da44c561add7e25debbb6ce036e125d8
solovyev.ed <evgeniy.solovyev@sbermarket.ru>
2024-01-10 19:13:47 +0300

    Demo tests

--- pages/login_admin_page.py
+++ pages/login_admin_page.py
@@ -8,13 +8,15 @@ class LoginAdminPage:
     SUBMIT_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
     OPENCART_LINK = (By.XPATH, "//*[text()='OpenCart']")
     FORGOTTEN_PASSWORD = (By.CSS_SELECTOR, "#input-password")
-
     PERCENT_ORDERS = (By.XPATH, "//*[text()='Total Orders ']/span[@class='float-end']")
     PERCENT_SALES = (By.XPATH, "//*[text()='Total Sales ']/span[@class='float-end']")
     PERCENT_CUSTOMERS = (By.XPATH, "//*[text()='Total Customers ']/span[@class='float-end']")
     PERSON_LABEL = (By.XPATH, "//span[@class='d-none d-md-inline d-lg-inline']")
-
-    BUTTON_LOGIUT = (By.XPATH, "//span[@class='d-none d-md-inline']")
-
-
-
+    BUTTON_LOGOUT = (By.XPATH, "//span[@class='d-none d-md-inline']")
+    UNSUCCESSIBLE_LOGIN_ALERT = (By.XPATH, "//*[@class='alert alert-danger alert-dismissible']")
+    UNSUCCESSIBLE_LOGIN_TEXT = (By.XPATH, "//i[@class='fa-solid fa-circle-exclamation']")
+    CUSTOMERS_MENU = (By.XPATH, "//*[@class='fas fa-user']")
+    CUSTOMERS = (By.XPATH, "// a[text() = 'Customers']")
+    SELECT_ALL_USERS = (By.XPATH, "//thead//input[@class='form-check-input']")
+    TRASH_ALL_USERS = (By.XPATH, "//*[@class='fa-regular fa-trash-can']")
+    LOGIN_FORM = (By.XPATH, "//div[@class='card-header']")
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,12 @@
-import pytest
+import time
+import random
+import string
 
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from pages.login_admin_page import LoginAdminPage
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -9,6 +16,8 @@ from selenium.webdriver.firefox.options import Options as FFOptions
 
 from selenium.webdriver.safari.service import Service as SafariService
 from selenium.webdriver.safari.options import Options as SafariOptions
+
+from pages.login_main_page import LoginMainPage
 
 
 @pytest.fixture()
@@ -35,9 +44,10 @@ def browser(request):
         driver = webdriver.Safari(service=service, options=options)
 
     # Set window size
-    width = 1920  # in pixels
-    height = 1080  # in pixels
-    driver.set_window_size(width, height)
+    # width = 1920  # in pixels
+    # height = 1080  # in pixels
+    # driver.set_window_size(width, height)
+    driver.maximize_window()
 
     request.addfinalizer(driver.close)
 
@@ -45,3 +55,31 @@ def browser(request):
     driver.url = url
 
     return driver
+
+
+@pytest.fixture()
+def admin_login(browser):
+    browser.get(browser.url + "/administration")
+    browser.find_element(*LoginAdminPage.USERNAME_INPUT).send_keys("user")
+    browser.find_element(*LoginAdminPage.PASSWORD_INPUT).send_keys("bitnami")
+    browser.find_element(*LoginAdminPage.SUBMIT_BUTTON).submit()
+
+@pytest.fixture()
+def main_login(browser):
+    browser.get(browser.url)
+    wait = WebDriverWait(browser, 5, poll_frequency=1)
+    wait.until(EC.title_is("Your Store"))
+    browser.find_element(*LoginMainPage.ACCOUNT_DROPDOWN).click()
+    browser.find_element(*LoginMainPage.LOGIN).click()
+    browser.find_element(*LoginMainPage.EMAIL).send_keys("um8mp@inbox.ru")
+    browser.find_element(*LoginMainPage.PASSWORD).send_keys("um8mp")
+    browser.find_element(*LoginMainPage.CONTINUE_BUTTON).click()
+    wait.until(
+        EC.visibility_of_element_located(LoginMainPage.LOGIN_PAGE))
+
+
+@pytest.fixture()
+def main_page(browser):
+    browser.get(browser.url)
+    wait = WebDriverWait(browser, 5, poll_frequency=1)
+    wait.until(EC.title_is("Your Store"))
